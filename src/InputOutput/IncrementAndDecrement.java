a9cadce3749fa279a13816739de651340c86ae03
Pankaj <rcypankaj@gmail.com>
2022-10-21 14:31:11 +0530

    Initial Commit

---
+++ src/InputOutput/IncrementAndDecrement.java
@@ -0,0 +1,14 @@
+package InputOutput;
+import java.util.Scanner;
+// Question URL: https://my.newtonschool.co/playground/code/hewtcm3na3uj/
+// Start
+public class IncrementAndDecrement {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short a = sc.nextShort();
+        short b = sc.nextShort();
+
+        System.out.print(a-1+" "+ (b+1));
+    }
+}
+// End
