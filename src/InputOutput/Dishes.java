a2998bb7f9fbf8376fbadd49ad7175aa4cdee98f
Pankaj <rcypankaj@gmail.com>
2022-10-21 15:51:39 +0530

    Initial Commit

---
+++ src/InputOutput/Dishes.java
@@ -0,0 +1,15 @@
+package InputOutput;
+
+import java.util.Scanner;
+// Question URL: https://my.newtonschool.co/playground/code/ofhnmh0gd6kq/
+// Start
+public class Dishes {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short n = sc.nextShort();
+        short t = sc.nextShort();
+
+        System.out.println(t - n);
+    }
+}
+// End
