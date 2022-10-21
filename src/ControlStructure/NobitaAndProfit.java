81307f0b1f6c49a915572edc91bc66cf890f5ac1
Pankaj <rcypankaj@gmail.com>
2022-10-21 16:05:08 +0530

    Initial Commit

---
+++ src/ControlStructure/NobitaAndProfit.java
@@ -0,0 +1,14 @@
+package ControlStructure;
+
+import java.util.Scanner;
+// Question URL: https://my.newtonschool.co/playground/code/mcl7rq7msatn/
+// Start
+public class NobitaAndProfit {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short c = sc.nextShort();
+        short s = sc.nextShort();
+        System.out.println(s - c);
+    }
+}
+// End
