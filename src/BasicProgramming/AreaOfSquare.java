c5978195fbc6feff09939707818e66a948ccc979
Pankaj <rcypankaj@gmail.com>
2022-10-21 12:50:09 +0530

    Initial Commit

---
+++ src/BasicProgramming/AreaOfSquare.java
@@ -0,0 +1,13 @@
+package BasicProgramming;
+
+import java.util.Scanner;
+
+// Given a side of a square, your task is to calculate its area.
+// Start
+public class AreaOfSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int side  = sc.nextInt();
+        System.out.println(side * side);
+    }
+}
