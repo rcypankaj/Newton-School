2426f5a965e85597ebabc1c815fa3254e9639644
Pankaj <rcypankaj@gmail.com>
2022-10-21 15:46:46 +0530

    Initial Commit

---
+++ src/InputOutput/SimpleSum.java
@@ -0,0 +1,16 @@
+package InputOutput;
+
+import java.util.Scanner;
+// URL of This Question: https://my.newtonschool.co/playground/code/n4zxx1f2gdip/
+// Start
+public class SimpleSum {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short a = sc.nextShort();
+        short b = sc.nextShort();
+        short c = sc.nextShort();
+
+        System.out.println(a+b+c);
+    }
+}
+// End
