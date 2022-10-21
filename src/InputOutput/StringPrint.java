a14809dc207aa03e9999a755de56eb7501d78b7a
Pankaj <rcypankaj@gmail.com>
2022-10-21 15:48:54 +0530

    Initial Commit

---
+++ src/InputOutput/StringPrint.java
@@ -0,0 +1,13 @@
+package InputOutput;
+
+import java.util.Scanner;
+// Question URL: https://my.newtonschool.co/playground/code/2xp0i530j73j/
+// Start
+public class StringPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+        System.out.println(S);
+    }
+}
+// End
