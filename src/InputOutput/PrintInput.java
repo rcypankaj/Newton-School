217bec864d5ec7aefc4539cb569bdd2b90899a11
Pankaj <rcypankaj@gmail.com>
2022-10-21 14:34:09 +0530

    Initial Commit

---
+++ src/InputOutput/PrintInput.java
@@ -0,0 +1,13 @@
+package InputOutput;
+
+import java.util.Scanner;
+// Question URL: https://my.newtonschool.co/playground/code/ienlv9z3lnqc/
+// Start
+public class PrintInput {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte n = sc.nextInt();
+        System.out.println(n);
+    }
+}
+// End
