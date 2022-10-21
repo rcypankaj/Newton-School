6c58dd45d3725d64889345a3e667daaf20c15e3b
Pankaj <rcypankaj@gmail.com>
2022-10-21 16:07:44 +0530

    Initial Commit

---
+++ src/ControlStructure/WaitingTime.java
@@ -0,0 +1,15 @@
+package ControlStructure;
+
+import java.util.Scanner;
+// Question URL: https://my.newtonschool.co/playground/code/6mwqpht2br67/
+// Start
+public class WaitingTime {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        System.out.println(n * x);
+    }
+}
+// End
