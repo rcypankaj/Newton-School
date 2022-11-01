a8bd250e6c01a71f1c92536f2100d507a647cd9c
Pankaj <rcypankaj@gmail.com>
2022-11-01 20:24:26 +0530

    Initial Commit

--- src/Sorting/LogicalSearch.java
+++ src/Sorting/LogicalSearch.java
@@ -4,19 +4,24 @@ import java.util.*;
 // Question URL: https://my.newtonschool.co/playground/code/922y7e3y33rq/
 // Start
 public class LogicalSearch {
-    public static int findMinOccurence(int[] arr, int n) {
-        for (int i = 0; i < n; i += 2) {
-            if (i == n-1) return arr[i];
-            else {
-                if (arr[i] != arr[i+1]) {
-                    return arr[i];
-                }
-            }
+    public static int findMissing(int[] arr, int n) {
+        int l = 0, h = n-1;
+        int diff = (arr[h] - arr[l])/n;
+        // System.out.println(diff);
+
+        //binary search
+        while (l <= h) {
+            int mid = l + (h-l)/2;
+            if (mid >= 0 && mid < n && arr[mid] - arr[mid-1] != diff)
+                return arr[mid]-diff;
+
+            if (arr[mid] - arr[0] == diff*mid)
+                l = mid+1;
+            else h = mid-1;
         }
         return -1;
     }
-
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
@@ -26,9 +31,8 @@ public class LogicalSearch {
             arr[i] = sc.nextInt();
         }
 
-        Arrays.sort(arr);
+        System.out.println(findMissing(arr,n));
 
-        System.out.println(findMinOccurence(arr,n));
     }
 }
 // End
